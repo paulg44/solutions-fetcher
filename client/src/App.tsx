5abdfb90221043bbdb4182501c15ad6f92102ab9
Paul Garton <paulgarton1984@hotmail.com>
2025-08-04 21:12:45 +0100

    basic set up and make sure tailwind is configured

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="">Kids Chores</h1>
+    </div>
+  );
+}
+
+export default App;
